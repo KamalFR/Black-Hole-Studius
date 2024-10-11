ac1988991a5d7159138c8e87215ffa40a7283223
Lucas Albuquerque <guinho.lucas7@gmail.com>
2024-10-11 14:24:46 -0300

    updates in project

--- Assets/Scripts/DetectaNave.cs
+++ Assets/Scripts/DetectaNave.cs
@@ -10,38 +10,16 @@ public class DetectaNave : MonoBehaviour
 
     private void Start()
     {
-        if (alertaImage != null)
-        {
-            corOriginal = alertaImage.color;
-            Debug.Log("Cor original armazenada: " + corOriginal);
-        }
-        else
-        {
-            Debug.LogError("AlertaImage não está atribuído!");
-        }
+        if (alertaImage) corOriginal = alertaImage.color;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("OnTriggerEnter2D chamado com " + other.name);
-
-        if (other.CompareTag("Nave"))
-        {
-            Debug.Log("Nave detectada. Mudando cor para: " + novaCor);
-            alertaImage.color = novaCor;
-            Debug.Log("Cor atual da alertaImage: " + alertaImage.color);
-        }
+        if (other.CompareTag("Nave")) alertaImage.color = novaCor;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("OnTriggerExit2D chamado com " + other.name);
-
-        if (other.CompareTag("Nave"))
-        {
-            Debug.Log("Nave saiu. Restaurando cor para: " + corOriginal);
-            alertaImage.color = corOriginal;
-            Debug.Log("Cor restaurada da alertaImage: " + alertaImage.color);
-        }
+        if (other.CompareTag("Nave")) alertaImage.color = corOriginal;
     }
 }
