e7aeabccc5a704813247197e89cd2e049ad5771c
DESKTOP-00M3VOM\Pedro Anicio <p.a.d.moraes2003@hotmail.com>
2024-10-12 10:54:05 -0300

    nave funcionando no cenário base nosso

--- Assets/Scripts/ScriptsMalerba/NaveController.cs
+++ Assets/Scripts/ScriptsMalerba/NaveController.cs
@@ -13,42 +13,42 @@ public class NaveController : MonoBehaviour
     {
         // verifica se o player se movimentou e qual a direção
         if(!isRotating){
-            Debug.Log("Abuble");
             transform.Translate(movediraction * moveSpeed);
         }
         else{
-            Debug.Log("Abuble");
            transform.Rotate(movediraction * rotationSpeed);
         }
     }
-    void Update()
-    {
-        // Movimenta a nave para frente (tecla W e S)
-        if (Input.GetKey(KeyCode.W))
-        {
-            isRotating = false;
-            movediraction = Vector3.up;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            isRotating = false;
-            movediraction = Vector3.down;
-        }
-        // Rotaciona a nave (A ou D)
-        else if (Input.GetKey(KeyCode.A))
-        {
-            isRotating = true;
-            movediraction = Vector3.forward;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            isRotating = true;
-            movediraction = -Vector3.forward;
-        }
-        // se o Player nao apertar nada
-        else{
-            isRotating = false;
-            movediraction = Vector3.zero;
-        }
-    }
+
+    //UPDATE PARA QUANDO ESTIVER NO CENÁRIO
+    // void Update()
+    // {
+    //     // Movimenta a nave para frente (tecla W e S)
+    //     if (Input.GetKey(KeyCode.W))
+    //     {
+    //         isRotating = false;
+    //         movediraction = Vector3.up;
+    //     }
+    //     else if (Input.GetKey(KeyCode.S))
+    //     {
+    //         isRotating = false;
+    //         movediraction = Vector3.down;
+    //     }
+    //     // Rotaciona a nave (A ou D)
+    //     else if (Input.GetKey(KeyCode.A))
+    //     {
+    //         isRotating = true;
+    //         movediraction = Vector3.forward;
+    //     }
+    //     else if (Input.GetKey(KeyCode.D))
+    //     {
+    //         isRotating = true;
+    //         movediraction = -Vector3.forward;
+    //     }
+    //     // se o Player nao apertar nada
+    //     else{
+    //         isRotating = false;
+    //         movediraction = Vector3.zero;
+    //     }
+    // }
 }
