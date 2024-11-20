793065cf99142629cf344d16f9cd820329808195
Lucas Albuquerque <guinho.lucas7@gmail.com>
2024-11-20 03:42:41 -0300

    correção de bugs2

--- Assets/Scripts/ScriptsGuinho/MenuTrigger.cs
+++ Assets/Scripts/ScriptsGuinho/MenuTrigger.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class MenuTrigger : MonoBehaviour
 {
     public float distance;
-    public bool isTerminal;
-    public bool isGas;
-
+    public bool isTask;
     public Transform playerCamera;
     public GameObject gasTask;
 
@@ -19,17 +17,17 @@ public class MenuTrigger : MonoBehaviour
     {
         if (Physics.Raycast(playerCamera.position, EngineAmount.instance.transform.TransformDirection(Vector3.forward), out RaycastHit hit, distance))
         {
-            if (hit.collider.tag == menuTag && (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E)) && isTerminal)
+            if (hit.collider.tag == menuTag && (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E)) && !isTask)
             {
                 terminal.SetActive(true);
             }
 
-            if (hit.collider.tag == "LinesPainel" + GameManager.instance._indexLineTask && (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E)) && !isTerminal && !isGas)
+            if (hit.collider.tag == "LinesPainel" + GameManager.instance._indexLineTask && (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E)) && isTask)
             {
                 GameManager.instance.linesTasks[GameManager.instance._indexLineTask].SetActive(true);
             }
 
-            if (hit.collider.tag == "TimeKill" && GameManager.instance._taskOxigenToDo && (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E)) && !isTerminal && isGas)
+            if (hit.collider.tag == "TimeKill" && GameManager.instance._taskOxigenToDo && (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.E)) && isTask)
             {
                 gasTask.SetActive(true);
             }
