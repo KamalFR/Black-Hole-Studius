53f4669d5bbe390dd1658b3cf1cc3476c58e9bcc
João Prado <jpdiasprado01@gmail.com>
2024-11-26 08:50:57 -0300

    [IMP] - adicionado variável para controle do alarme

--- Assets/Scripts/Light/LightManager.cs
+++ Assets/Scripts/Light/LightManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.ProBuilder;
 
 public class LightManager : MonoBehaviour
 {
+    public bool StartAlarmLight;
+    private bool _alarmTurnedOff;
     [SerializeField] private List<Light> _shipLights;
+    private Color _normalShipLightColor;
+    private float _normalShipLightIntensity;
 
     [SerializeField] private float _maxShipLightIntensity;
     [SerializeField] private float _minShipLightIntensity;
@@ -17,13 +22,28 @@ public class LightManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartAlarmLight = false;
+        _alarmTurnedOff = true;
         _fixedShipLightIntensity = (_maxShipLightIntensity + _minShipLightIntensity) / 2;
         GetShipLights();
+        if (_shipLights.Count > 0)
+        {
+            _normalShipLightColor = _shipLights[0].color;
+            _normalShipLightIntensity = _shipLights[0].intensity;
+        }
     }
 
     void FixedUpdate()
     {
-        StartAlarmLights();
+        if (StartAlarmLight)
+        {
+            _alarmTurnedOff = false;
+            StartAlarmLights();
+        }
+        else if (!_alarmTurnedOff)
+        {
+            TurnOffAlarm();
+        }
     }
 
     void StartAlarmLights()
@@ -64,5 +84,15 @@ public class LightManager : MonoBehaviour
         {
             _shipLights.Add(objectLight.GetComponent<Light>());
         }
+    }
+
+    void TurnOffAlarm()
+    {
+        foreach (var shipLight in _shipLights)
+        {
+            shipLight.color = _normalShipLightColor;
+            shipLight.intensity = _normalShipLightIntensity;
+        }
+        _alarmTurnedOff = true;
     }
 }
